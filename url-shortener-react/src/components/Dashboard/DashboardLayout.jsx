07295e006e8ed46c0af51101f35f542f6e010ad4
Suyash Baoney <suyashbaoney58@gmail.com>
2025-06-05 15:16:19 +0530

    message overlap issue resolved for Dashboard

--- url-shortener-react/src/components/Dashboard/DashboardLayout.jsx
+++ url-shortener-react/src/components/Dashboard/DashboardLayout.jsx
@@ -68,12 +68,13 @@ const DashboardLayout = () => {
          </div>
 
           {/* 🔹 Analytics Graph */}
+          <div className="relative min-h-[300px] flex flex-col items-center justify-between">
           {totalClicks && totalClicks.length === 0 ? (
-            <div className="absolute flex flex-col justify-center sm:items-center items-end w-full left-0 top-0 bottom-0 right-0 m-auto">
-              <h1 className="text-slate-800 font-serif sm:text-2xl text-[18px] font-bold mb-1">
+            <div className="text-center">
+              <h1 className="text-slate-800 font-serif sm:text-2xl text-[18px] font-bold mb-2">
                 No Data For This Time Period
               </h1>
-              <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-sm text-slate-600">
+              <h3 className="sm:w-96 w-[90%] text-center sm:text-lg text-sm text-slate-600">
                 Share your short link to view where your engagements are coming from.
               </h3>
             </div>
@@ -82,6 +83,7 @@ const DashboardLayout = () => {
               <Graph graphData={totalClicks} />
             </div>
           )}
+          </div>
 
           {/* 🔹 Shorten URL Button */}
           <div className='py-5 sm:text-end text-center'>
