3af9b5cc91b9e416e747dc75189e37ad45ac9a04
Suyash <144018815+suyash-rgb@users.noreply.github.com>
2026-01-02 00:58:50 +0530

    Fix rendering of ShortenUrlList component

--- url-shortener-react/src/components/Dashboard/ShortenUrlList.jsx
+++ url-shortener-react/src/components/Dashboard/ShortenUrlList.jsx
@@ -3,16 +3,16 @@ import ShortenItem from './ShortenItem'
 
 const ShortenUrlList = ({ data, refetch, startDate, endDate }) => {
   return (
-    <div className='my-6 space-y-4'>
-        {data.map((item) => (
-            <ShortenItem 
-              key={item.id} 
-              {...item} 
-              refetch={refetch}
-              startDate={startDate} 
-              endDate={endDate}  
-            />
-        ))}
+    <div className="my-6 space-y-4">
+      {(data || []).map((item) => (
+        <ShortenItem
+          key={item.id}
+          {...item}
+          refetch={refetch}
+          startDate={startDate}
+          endDate={endDate}
+        />
+      ))}
     </div>
   )
 }
