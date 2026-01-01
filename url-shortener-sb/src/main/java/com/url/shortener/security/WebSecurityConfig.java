b95c5367eb1622e5aa51656c40b329419b47d878
Suyash <144018815+suyash-rgb@users.noreply.github.com>
2026-01-02 00:27:56 +0530

    resolving import error

--- url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+import java.util.List;
 import java.util.Arrays;
 
 @Configuration
